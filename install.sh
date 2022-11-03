039fc3a265afad0a86e175dc1e3cceb704f153ff
JohansenJunias20 <johansen.gumbal@gmail.com>
2022-11-03 20:46:31 +0700

    typo

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 # installing so can start on boot linux
 sudo cp rustdesk.service /etc/systemd/system/rustdesk.service
 sed -i "s,__dir__,$PWD," /etc/systemd/system/rustdesk.service
-chmod 777 start_startup.sh
+chmod 777 start.sh
 systemctl enable rustdesk
